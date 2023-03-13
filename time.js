9aa36205cc884245fd13d9915d13733cde657039
youssifahmed819@gmail.com <youssifahmed819@gmail.com>
2023-03-13 21:19:50 +0200

    s

---
+++ time.js
@@ -0,0 +1,4 @@
+/*
+and i started gaining experience since (date) by making lots of templates ,, which means 
+i've been working for (timeInDays) days, or you can say almost (timeInMonth) months
+*/
